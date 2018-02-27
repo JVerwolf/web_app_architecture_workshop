ce47af137385f94d48078ef2b1c64b4d3519947b
John Verwolf <jdverwolf@gmail.com>
2018-02-26 17:09:53 -0800

    Fix formatting

--- static_server_example/app.js
+++ static_server_example/app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 // BodyParser is Express middleware. It parses elements out
 // of the request string and adds them to the req object.
-// We don't need this for what we are doing, but these
+// We don't need this for what we are doing, but this will
 // allow you play around with POST requests.
 app.use(bodyParser.json());
 
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 // This defines a resource at "/" for "GET" on our API.
 app.get('/', (req, res) => {
-    res.sendFile(__dirname +  '/index.html');
+    res.sendFile(__dirname + '/index.html');
     console.log("We just got a GET request at '/' in our API!");
 });
 
